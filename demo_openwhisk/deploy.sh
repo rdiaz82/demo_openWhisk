3efe76423fcbc9ee75653f644e1528636ece69d3
Roberto Diaz <roberto@theagilemonkeys.com>
2018-10-20 01:44:30 +0100

    refactor in project files name

--- demo_openwhisk/deploy.sh
+++ demo_openwhisk/deploy.sh
@@ -3,6 +3,6 @@
 #Action: Save Measurement to DynamoDb.
 mkdir ./temp
 cd temp
-cp ./target/x86_64-unknown-linux-musl/release/main ./exec
+cp ./target/x86_64-unknown-linux-musl/release/save_data ./exec
 zip save_data.zip ./exec
 wsk action create putMeasurement ./save_data.zip --native --param AWS_KEY $AWS_ACCESS_KEY_ID --param AWS_SECRET $AWS_SECRET_ACCESS_KEY  -i
