7a14104626a48f6ab6e3936ee02d059fe4e8d75d
Roberto Diaz <roberto@theagilemonkeys.com>
2018-10-20 01:36:23 +0100

    refactor in project structure

--- demo_openwhisk/deploy.sh
+++ demo_openwhisk/deploy.sh
@@ -2,6 +2,7 @@
 
 #Action: Save Measurement to DynamoDb.
 mkdir ./temp
-cp ./target/x86_64-unknown-linux-musl/release/save_data ./temp/exec
-zip ./temp/save_data.zip exec
-wsk action create putMeasurement ./temp/save_data.zip --native --param AWS_KEY $AWS_ACCESS_KEY_ID --param AWS_SECRET $AWS_SECRET_ACCESS_KEY  -i
+cd temp
+cp ./target/x86_64-unknown-linux-musl/release/main ./exec
+zip save_data.zip ./exec
+wsk action create putMeasurement ./save_data.zip --native --param AWS_KEY $AWS_ACCESS_KEY_ID --param AWS_SECRET $AWS_SECRET_ACCESS_KEY  -i
--- demo_openwhisk/src/bin/main.rs
+++ demo_openwhisk/src/bin/main.rs
@@ -1,5 +1,78 @@
 extern crate core;
 
+extern crate chrono;
+extern crate openssl_probe;
+extern crate rusoto_core;
+extern crate rusoto_dynamodb;
+extern crate rustc_serialize;
+extern crate uuid;
+
+use chrono::prelude::*;
+use rusoto_core::Region;
+use rusoto_dynamodb::{AttributeValue, DynamoDb, DynamoDbClient, PutItemInput};
+use rustc_serialize::json;
+use rustc_serialize::json::Json;
+use std::collections::HashMap;
+use std::default::Default;
+use std::env;
+use uuid::Uuid;
+
 fn main() {
-    core::save_data();
+    core::set_env();
+    println!("{:?}", env::var("AWS_SECRET_ACCESS_KEY"));
+    let mut measurement = "0".to_string();
+    if let Some(arg1) = env::args().nth(1) {
+        let params = Json::from_str(&arg1).unwrap();
+        if let Some(params_obj) = params.as_object() {
+            if let Some(params_name) = params_obj.get("measurement") {
+                measurement = params_name.as_string().unwrap().to_string();
+            }
+        }
+    };
+
+    let client = DynamoDbClient::new(Region::UsEast2);
+    let mut item: HashMap<String, AttributeValue> = HashMap::new();
+    item.insert(
+        "id".to_string(),
+        AttributeValue {
+            s: Some(Uuid::new_v4().to_string()),
+            ..Default::default()
+        },
+    );
+    item.insert(
+        "measurement".to_string(),
+        AttributeValue {
+            n: Some(measurement),
+            ..Default::default()
+        },
+    );
+    item.insert(
+        "timestamp".to_string(),
+        AttributeValue {
+            s: Some(Utc::now().to_string()),
+            ..Default::default()
+        },
+    );
+
+    let put_item = PutItemInput {
+        table_name: "Measurements".to_string(),
+        item: item,
+        ..Default::default()
+    };
+
+    match client.put_item(put_item).sync() {
+        Ok(_) => {
+            let out = core::Output {
+                message: "ok".to_string(),
+            };
+            println!("{}", json::encode(&out).unwrap())
+        }
+        Err(error) => {
+            let out = core::Output {
+                message: error.to_string(),
+            };
+            println!("{}", json::encode(&out).unwrap())
+        }
+    };
 }
+
