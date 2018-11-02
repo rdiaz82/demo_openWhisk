e062f9232033e6e0c627a47e5dd062db87d823ba
Roberto Diaz <roberto@theagilemonkeys.com>
2018-11-02 02:17:21 +0000

    optimized the deploy script to cache the Rust libraries to reduce the compilation time

--- demo_openwhisk/deploy.sh
+++ demo_openwhisk/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-alias rust-musl-builder='docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder'
+alias rust-musl-builder='docker run --rm -it -v "$(pwd)":/home/rust/src  -v cargo-git:/home/rust/.cargo/git -v cargo-registry:/home/rust/.cargo/registry ekidd/rust-musl-builder'
+rust-musl-builder sudo chown -R rust:rust /home/rust/.cargo/git /home/rust/.cargo/registry
 rust-musl-builder cargo build --release
 
 wsk action delete putMeasurement -i
